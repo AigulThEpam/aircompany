1f7221df1674d3d808826c3d2a61a58fc4681643
AigulThEpam <113446729+AigulThEpam@users.noreply.github.com>
2024-12-17 13:38:06 +0000

    Fixed according to comments

--- Java/src/main/java/Airport.java
+++ Java/src/main/java/Airport.java
@@ -8,6 +8,7 @@ import java.util.ArrayList;
 import java.util.Collections;
 import java.util.Comparator;
 import java.util.List;
+import java.util.stream.Collectors;
 
 // version: 1.1
 // made by Vitali Shulha
@@ -16,73 +17,50 @@ import java.util.List;
 public class Airport {
     private List<? extends Plane> planes;
 
-    public List<PassengerPlane> getPasPl() {
+    public List<PassengerPlane> getPassengerPlane() {
         List<? extends Plane> l = this.planes;
-        List<PassengerPlane> x = new ArrayList<>();
-        for (Plane p : l) {
-            if (p instanceof PassengerPlane) {
-                x.add((PassengerPlane) p);
-            }
-        }
-        return x;
+        return l.stream()
+                .filter(PassengerPlane.class::isInstance)
+                .map(PassengerPlane.class::cast)
+                .collect(Collectors.toList());
     }
 
     public List<MilitaryPlane> getMilitaryPlanes() {
-        List<MilitaryPlane> militaryPlanes = new ArrayList<>();
-        for (Plane plane : planes) {
-            if (plane instanceof MilitaryPlane) {
-                militaryPlanes.add((MilitaryPlane) plane);
-            }
-        }
-        return militaryPlanes;
+        return planes.stream()
+                .filter(MilitaryPlane.class::isInstance)
+                .map(MilitaryPlane.class::cast)
+                .collect(Collectors.toList());
     }
 
     public PassengerPlane getPassengerPlaneWithMaxPassengersCapacity() {
-        List<PassengerPlane> passengerPlanes = getPasPl();
-        PassengerPlane planeWithMaxCapacity = passengerPlanes.get(0);
-        for (int i = 0; i < passengerPlanes.size(); i++) {
-            if (passengerPlanes.get(i).getPassengersCapacity() > planeWithMaxCapacity.getPassengersCapacity()) {
-                planeWithMaxCapacity = passengerPlanes.get(i);
-            }
-        }
-        return planeWithMaxCapacity;
+        List<PassengerPlane> passengerPlanes = getPassengerPlane();
+
+        return passengerPlanes.stream()
+                .max(Comparator.comparing(PassengerPlane::getPassengersCapacity))
+                .orElse(null);
     }
 
     public List<MilitaryPlane> getTransportMilitaryPlanes() {
-        List<MilitaryPlane> transportMilitaryPlanes = new ArrayList<>();
         List<MilitaryPlane> militaryPlanes = getMilitaryPlanes();
 
-        for (int i = 0; i < militaryPlanes.size(); i++) {
-            MilitaryPlane plane = militaryPlanes.get(i);
-            if (plane.getType() == MilitaryType.TRANSPORT) {
-                transportMilitaryPlanes.add(plane);
-            }
-        }
-        return transportMilitaryPlanes;
+        return militaryPlanes.stream()
+                .filter(plane -> plane.getType()== MilitaryType.TRANSPORT)
+                .collect(Collectors.toList());
     }
 
     public List<MilitaryPlane> getBomberMilitaryPlanes() {
-        List<MilitaryPlane> bomberMilitaryPlanes = new ArrayList<>();
         List<MilitaryPlane> militaryPlanes = getMilitaryPlanes();
 
-        for (int i = 0; i < militaryPlanes.size(); i++) {
-            MilitaryPlane plane = militaryPlanes.get(i);
-            if (plane.getType() == MilitaryType.BOMBER) {
-                bomberMilitaryPlanes.add(plane);
-            }
-        }
-        return bomberMilitaryPlanes;
+        return militaryPlanes.stream()
+                .filter(plane -> plane.getType()== MilitaryType.BOMBER)
+                .collect(Collectors.toList());
     }
 
     public List<ExperimentalPlane> getExperimentalPlanes() {
-        List<ExperimentalPlane> experimentalPlanes = new ArrayList<>();
-
-        for (Plane plane : planes) {
-            if (plane instanceof ExperimentalPlane) {
-                experimentalPlanes.add((ExperimentalPlane) plane);
-            }
-        }
-        return experimentalPlanes;
+        return planes.stream()
+                .filter(ExperimentalPlane.class::isInstance)
+                .map(ExperimentalPlane.class::cast)
+                .collect(Collectors.toList());
     }
 
     public Airport sortByMaxDistance() {
--- Java/src/main/java/com/planes/Plane.java
+++ Java/src/main/java/com/planes/Plane.java
@@ -1,60 +1,17 @@
 package com.planes;
 
-import java.util.Objects;
+import lombok.AllArgsConstructor;
+import lombok.EqualsAndHashCode;
+import lombok.Getter;
+import lombok.ToString;
 
-abstract public class Plane {
-    String model;
+@Getter
+@AllArgsConstructor
+@ToString
+@EqualsAndHashCode
+public abstract class Plane {
+    public String model;
     private int maxSpeed;
     private int maxFlightDistance;
     private int maxLoadCapacity;
-
-    public Plane(String model, int maxSpeed, int maxFlightDistance, int maxLoadCapacity) {
-        this.model = model;
-        this.maxSpeed = maxSpeed;
-        this.maxFlightDistance = maxFlightDistance;
-        this.maxLoadCapacity = maxLoadCapacity;
-    }
-
-    public String getModel() {
-        return model;
-    }
-
-    public int getMaxSpeed() {
-        return maxSpeed;
-    }
-
-    public int getMaxFlightDistance() {
-        return maxFlightDistance;
-    }
-
-    public int getMaxLoadCapacity() {
-        int result = this.maxLoadCapacity;
-        return result;
-    }
-
-    @Override
-    public String toString() {
-        return "Plane{" +
-                "model='" + model + '\'' +
-                ", maxSpeed=" + maxSpeed +
-                ", maxFlightDistance=" + maxFlightDistance +
-                ", maxLoadCapacity=" + maxLoadCapacity +
-                '}';
-    }
-
-    @Override
-    public boolean equals(Object o) {
-        if (this == o) return true;
-        if (!(o instanceof Plane)) return false;
-        Plane plane = (Plane) o;
-        return maxSpeed == plane.maxSpeed &&
-                maxFlightDistance == plane.maxFlightDistance &&
-                maxLoadCapacity == plane.maxLoadCapacity &&
-                Objects.equals(model, plane.model);
-    }
-
-    @Override
-    public int hashCode() {
-        return Objects.hash(model, maxSpeed, maxFlightDistance, maxLoadCapacity);
-    }
 }
